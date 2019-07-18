cfbcf808262d9d1047cd1dd56cbe944a05e00354
Karol Żurek <karczi9411@gmail.com>
2019-07-18 22:25:23 +0200

    Initial commit

---
+++ Assets/NowaKulka.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NowaKulka : MonoBehaviour
+{
+
+	public GameObject kulka;
+	public float pozycjax;
+	public float pozycjay;
+	public float pozycjaz;
+	public bool zarzadzanie;
+
+	//pobranie pozycji poczatkowych
+	void Start()
+	{
+		pozycjax = kulka.transform.position.x;
+		pozycjay = kulka.transform.position.y;
+		pozycjaz = kulka.transform.position.z;
+	}
+
+	void Update()
+	{
+		//jesli kulka przekroczy pozycje 
+		if (kulka.GetComponent<Transform>().position.x <= -25 || kulka.GetComponent<Transform>().position.x >= 20 || kulka.GetComponent<Transform>().position.z <= -5 || kulka.GetComponent<Transform>().position.z >= 10)
+		{
+			//wroci do pozycji poczatkowych
+			kulka.transform.position = new Vector3(pozycjax, pozycjay, pozycjaz);
+		}
+
+		//wylaczenie kinematyki
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			kulka.GetComponent<Rigidbody>().isKinematic = false;
+		}
+	}
+}
---
+++ Assets/Wiatr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiatr : MonoBehaviour
+{
+	public float silawiatru = 5;
+	public float zasieg = 100;
+	int i;
+	public Transform pozycjawiatru;
+	public Transform pozcyjarotacjiwiatru;
+	private RaycastHit hit;
+
+	void Update()
+	{
+		//wyjscie aplikacji po escape
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+
+			if (pozycjawiatru != null) //&& pozcyjarotacjiwiatru != null)
+		{
+			//pozcyjarotacjiwiatru.rotation = transform.rotation;
+
+			//zasieg kolizji
+			var hitColliders = Physics.OverlapSphere(pozycjawiatru.transform.position, zasieg);
+			for (i = 0; i < hitColliders.Length; i++)
+			{
+				if (hitColliders[i].GetComponent<Rigidbody>() != null)
+				{
+					//przechodzenie przez inne obiekty , raydirection
+					var rayDirection = hitColliders[i].GetComponent<Rigidbody>().gameObject.transform.position - pozycjawiatru.transform.position;
+					if (Physics.Raycast(pozycjawiatru.transform.position, rayDirection, out hit)) 
+					{
+						if (hit.transform.GetComponent<Rigidbody>())
+						{
+							//dzialanie sily poprzez hit coliders na inne obiekty
+							hitColliders[i].GetComponent<Rigidbody>().AddForce(pozycjawiatru.transform.forward * silawiatru, ForceMode.Acceleration);						
+						}
+					}
+				}
+			}
+		}
+
+	}
+}
